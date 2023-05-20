0b2c4f123f26d9288bd1804ac457ff3a539385a8
bino1999 <101051395+bino1999@users.noreply.github.com>
2023-05-20 13:06:40 +0530

    j
    jklm

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// app.js
+
 
 const express = require('express');
 const connectDB = require('./config/db');
@@ -35,46 +35,3 @@ const PORT = process.env.PORT || 5001;
 app.use(express.static(path.join(__dirname, '/client/build')))
    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-// app.get('/', (req, res) => {
-//    res.sendFile(path.join(__dirname, '/client/build/index.html'));
-// });
-
-// app.get('/authenticate', auth, (req, res) => {
-//    const options = {
-//       httpOnly: true,
-//       signed: true,
-//    };
-
-//    console.log(req.auth.user);
-
-//    if (req.auth.user === 'admin') {
-//       res.cookie('name', 'admin', options).send({ screen: 'admin' });
-//    } else if (req.auth.user === 'user') {
-//       res.cookie('name', 'user', options).send({ screen: 'user' });
-//    }
-// });
-
-// app.get('/read-cookie', (req, res) => {
-//    console.log(req.signedCookies);
-//    if (req.signedCookies.name === 'admin') {
-//       res.send({ screen: 'admin' });
-//    } else if (req.signedCookies.name === 'user') {
-//       res.send({ screen: 'user' });
-//    } else {
-//       res.send({ screen: 'auth' });
-//    }
-// });
-
-// app.get('/clear-cookie', (req, res) => {
-//    res.clearCookie('name').end();
-// });
-
-// app.get('/get-data', (req, res) => {
-//    if (req.signedCookies.name === 'admin') {
-//       res.send('This is admin panel');
-//    } else if (req.signedCookies.name === 'user') {
-//       res.send('This is user data');
-//    } else {
-//       res.end();
-//    }
-// });
